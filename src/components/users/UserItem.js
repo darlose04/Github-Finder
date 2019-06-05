12c56934681cf6c21f669a8aa0bd4aff0eb7589e
Zach Smith <zsmith126028@gmail.com>
2019-06-05 12:43:57 -0500

    remove state and destructure from props. state moved to Users

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -12,15 +12,16 @@ class UserItem extends Component {
   //   };
   // }
 
-  state = {
-    id: "id",
-    login: "mojombo",
-    avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
-    html_url: "https://github.com/mojombo"
-  };
+  // STATE MOVES TO THE USERS.JS
+  // state = {
+  //   id: "id",
+  //   login: "mojombo",
+  //   avatar_url: "https://avatars0.githubusercontent.com/u/1?v=4",
+  //   html_url: "https://github.com/mojombo"
+  // };
 
   render() {
-    const { login, avatar_url, html_url } = this.state;
+    const { login, avatar_url, html_url } = this.props.user; // this comes from the prop of user that was passed into the UserItem tag in Users.js
     return (
       <div className='card text-center'>
         <img
