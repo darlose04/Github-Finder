39bbedc511899b074bd35e1b2886578948765b35
Zach Smith <zsmith126028@gmail.com>
2019-06-05 17:35:14 -0500

    import Alert. create functionality for alert

--- src/App.js
+++ src/App.js
@@ -3,13 +3,15 @@ import Navbar from "./components/layout/Navbar";
 // import UserItem from "./components/users/UserItem";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
+import Alert from "./components/layout/Alert";
 import axios from "axios";
 import "./App.css";
 
 class App extends Component {
   state = {
     users: [],
-    loading: false
+    loading: false,
+    alert: null
   };
 
   // Don't need this anymore
@@ -43,16 +45,27 @@ class App extends Component {
     this.setState({ users: [], loading: false });
   };
 
+  // set alert
+  setAlert = (msg, type) => {
+    this.setState({ alert: { msg: msg, type: type } }); // can also just do {msg, type} because of es6
+
+    setTimeout(() => {
+      this.setState({ alert: null });
+    }, 5000);
+  };
+
   render() {
-    const { users, loading } = this.state;
+    const { users, loading, alert } = this.state;
     return (
       <div className='App'>
         <Navbar />
         <div className='container'>
+          <Alert alert={alert} />
           <Search
             searchUsers={this.searchUsers}
             clearUsers={this.clearUsers}
             showClear={users.length > 0 ? true : false}
+            setAlert={this.setAlert}
           />
           <Users loading={loading} users={users} />
         </div>
