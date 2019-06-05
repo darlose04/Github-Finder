c7ecf9770a7ebf10aae2c9a5d2a7194c5424b148
Zach Smith <zsmith126028@gmail.com>
2019-06-05 16:32:49 -0500

    add onChange and onSubmit

--- src/components/users/Search.js
+++ src/components/users/Search.js
@@ -1,11 +1,33 @@
 import React, { Component } from "react";
 
 class Search extends Component {
+  // this state doesn't need to be App level, it's relevant to this component
+  state = {
+    text: ""
+  };
+
+  // if you don't use arrow functions, you have to explicitly bind 'this'
+  // this.onSubmit.bind(this) below in the form tag
+
+  onSubmit = e => {
+    e.preventDefault(); // using this prevents the default action of it submitting to a file
+  };
+
+  onChange = e => {
+    this.setState({ [e.target.name]: e.target.value });
+  };
+
   render() {
     return (
       <div>
-        <form className='form'>
-          <input type='text' name='text' placeholder='Search Users...' />
+        <form onSubmit={this.onSubmit} className='form'>
+          <input
+            type='text'
+            name='text'
+            placeholder='Search Users...'
+            value={this.state.text}
+            onChange={this.onChange}
+          />
           <input
             type='submit'
             value='Search'
