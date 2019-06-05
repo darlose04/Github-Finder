d5892cc6d2fb0174f44ee2d147a18b0a180aa459
Zach Smith <zsmith126028@gmail.com>
2019-06-05 14:59:21 -0500

    destructure props as function arg

--- src/components/users/UserItem.js
+++ src/components/users/UserItem.js
@@ -1,9 +1,9 @@
-import React, { Component } from "react";
+import React from "react";
 
 // change class to functional component since it does not need state (until hooks were introduced, state could not be used with functional components)
 // no longer need to use 'this' since it is just a function and not a class
 // class UserItem extends Component {}
-const UserItem = props => {
+const UserItem = ({ user: { login, avatar_url, html_url } }) => {
   // two different ways of doing state
   // constructor(props) {
   //   super(props);
@@ -23,7 +23,8 @@ const UserItem = props => {
   //   html_url: "https://github.com/mojombo"
   // };
 
-  const { login, avatar_url, html_url } = props.user; // this comes from the prop of user that was passed into the UserItem tag in Users.js
+  // this is no longer needed because it can be destructured as an argument above
+  // const { login, avatar_url, html_url } = props.user; // this comes from the prop of user that was passed into the UserItem tag in Users.js
   return (
     <div className='card text-center'>
       <img
